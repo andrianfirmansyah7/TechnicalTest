2090261ef1359415d46158be86ebedad1bdb1cd9
andrianfirmansyah7 <andrianfirmansyah@student.telkomuniversity.ac.id>
2023-05-29 02:37:57 +0700

    Program tes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Soal Nomer 3</h1>
+  <div class="wrapper">
+    <div>
+    <div class="form">
+      <a href="#" @click="add" class="btn btn-primary" style="margin-bottom:10px"><i class="fa fa-plus"></i> Tambah</a>
+      <!-- Melakukan proses perulangan berdasarkan angka pada variabel jumlah -->
+      <div v-for="key in jumlah" :key="key">
+      <div class="row" style="margin-bottom:10px">
+      <div class="col-5">
+      <select v-model="values['dynamic-field-'+key]" :id="key" class="form-control">
+       <option default>Age</option>
+       <option>Education</option>
+       <option>Interest</option>
+       <option>Language</option>
+       <option>Marital Status</option>
+       <option>Skill</option>
+      </select>
+      </div>
+      <div class="col-5">
+        <input type="text" :id="key" class="form-control" placeholder="Isian">
+      </div>
+      <div class="col-2">
+        <a href="#" @click="remove" class="btn btn-danger"><i class="fa fa-plus"></i> X</a>
+      </div>
+      </div>
+      </div>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+      jumlah: 1,
+     values: {}
+
+    }
+  },
+   methods: {
+   // Fungsi penambahan jumlah
+    add: function(){
+      this.jumlah++;
+    },
+    // Fungsi pengurangan jumlah
+    remove: function () {
+     this.jumlah--;
+
+    }
+    }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +62,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+
+body {
+  background-color: #f0f5ff;
+  color: #fff;
+}
+
+.form {
+  width: 450px;
+  margin: 15px auto;
+}
+
+h1 {
+  text-align: center;
+  font-size: 48px;
+  color: #232c3d;
 }
 </style>
---
+++ src/nomer1.vue
@@ -0,0 +1,40 @@
+<script>
+const employees = [
+    { "nik":"1", "name":"ABIYYU" },
+    { "nik":"2", "name":"ADHIKA" },
+    { "nik":"3", "name":"AFRIE" },
+    { "nik":"4", "name":"ALAM"}
+    ];
+
+const selected_employees = [
+    { "nik": "1", "name": "ABIYYU" },
+    { "nik": "2","name":"ADHIKA"}
+    ];
+
+const unselected_employees = [];
+
+  export default {
+  el: '#',
+  data() {
+    this.employees = employees;
+    this.selected_employees = selected_employees;
+    this.unselected_employees = unselected_employees;
+  },
+  method : {
+    show : function(){
+      for (let i = 0; i < this.employees.length; i++){
+        // Melakukan perbandingan
+        if(this.employees[i]["nik"] != this.selected_employees){
+          //Proses input data karyawan tidak terpilih ke dalam array
+          this.unselected.employees.push(this.employees[i]);
+        }
+      }
+      return this.unselected_employee;
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 id="data">{{ show() }}</h1>
+</template>
